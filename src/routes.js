5e2b3f8d895149d44fb3145f6777aae1a80186fb
elychr <51218224+elychr@users.noreply.github.com>
2020-12-15 13:23:14 +1100

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,4 @@
-let accounts = [
-    { label: 'Accounts Receivable', route: '/accredo/accounts/accounts-receivable', source: require('./pages/accredo_accounts_receivable.md') },
-    { label: 'Accounts Payable', route: '/accredo/accounts/accounts-payable', source: require('./pages/accredo_accounts_payable.md') },
-    { label: 'Cash Book', route: '/accredo/accounts/cashbook', source: require('./pages/accredo_cash_book.md') },
-    { label: 'General Ledger', route: '/accredo/accounts/ledger', source: require('./pages/accredo_general_ledger.md') },
-    { label: 'Goods And Services Tax', route: '/accredo/accounts/gst', source: require('./pages/accredo_gst.md') },
-  ]
-  
-  let accredo = [
-    { label: 'Accounting', route: '/accredo/accounts', children: accounts },
+   let accredo = [
     { label: 'Inventory Control', route: '/accredo/products', source: require('./pages/accredo_prods.md') },
     { label: 'Invoicing System', route: '/accredo/sales', source: require('./pages/accredo_sales.md') },
     { label: 'Purchase Orders', route: '/accredo/purchasing', source: require('./pages/accredo_purchase_orders.md') },
